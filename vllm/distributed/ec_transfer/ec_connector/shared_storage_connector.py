f4a8120584802146ed1dc4d63350a9c0a3cc3837
wuhang <wuhang6@huawei.com>
2025-09-16 06:35:57 +0000

    epd shm
    Signed-off-by: wuhang <wuhang6@huawei.com>

--- vllm/distributed/ec_transfer/ec_connector/shared_storage_connector.py
+++ vllm/distributed/ec_transfer/ec_connector/shared_storage_connector.py
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 import os
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 import safetensors
 
@@ -93,7 +93,7 @@ class ECSharedStorageConnector(ECConnectorBase):
                                    filename)
                     continue
                 ec_cache = safetensors.torch.load_file(
-                    filename)["ec_cache"].cuda()
+                    filename)["ec_cache"].npu()
                 if mm_data.request_id not in encoder_cache:
                     encoder_cache[mm_data.request_id] = {}
                 encoder_cache[mm_data.request_id][input_id] = ec_cache
@@ -134,18 +134,27 @@ class ECSharedStorageConnector(ECConnectorBase):
     def check_caches_exist(
         self,
         request: "Request",
+        index: Optional[int] = None,
     ) -> list[bool]:
         """
         Check if cache exist externally for each mm_data of request
         
         Args:
             request (Request): the request object.
+            index (Optional[int]): the index of the request in the batch.
 
         Returns:
             List of bool indicate that ith mm_data exist in cache or not
         """
         result = []
         request_id = request.request_id
+        if index is not None:
+            if self._found_match_for_mm_data(f"{request_id}_{index}"):
+                result.append(True)
+            else:
+                result.append(False)
+            return result
+
         for input_id in range(len(request.mm_positions)):
             if self._found_match_for_mm_data(f"{request_id}_{input_id}"):
                 result.append(True)
