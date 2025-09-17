b24bd233eddf016127099b35e06a3948a25cdcd0
wuhang <wuhang6@huawei.com>
2025-09-17 01:57:39 +0000

    epd shm
    Signed-off-by: wuhang <wuhang6@huawei.com>

--- examples/online_serving/epd/launch.sh
+++ examples/online_serving/epd/launch.sh
@@ -19,8 +19,8 @@ ENCODE_PORT=19534
 PREFILL_DECODE_PORT=19535
 PROXY_PORT=10001
 
-GPU_E=1
-GPU_PD=7
+GPU_E=0
+GPU_PD=1
 
 START_TIME=$(date +"%Y%m%d_%H%M%S")
 ENC_LOG="$LOG_PATH/encoder.log"
@@ -28,65 +28,82 @@ PD_LOG="$LOG_PATH/pd.log"
 PROXY_LOG="$LOG_PATH/proxy.log"
 PID_FILE="./pid.txt"
 
-SHARED_STORAGE_PATH="/path/to/your/share/storage"
+SHARED_STORAGE_PATH="/dev/shm/epd"
 
 ###############################################################################
 # Encoder worker
 ###############################################################################
-CUDA_VISIBLE_DEVICES="$GPU_E" vllm serve "$MODEL" \
-    --gpu-memory-utilization 0.0 \
-    --port "$ENCODE_PORT" \
-    --enable-request-id-headers \
-    --no-enable-prefix-caching \
-    --max-num-seqs 128 \
-    --enforce-eager \
-    --ec-transfer-config '{
-        "ec_connector": "ECSharedStorageConnector",
-        "ec_role": "ec_producer",
-        "ec_connector_extra_config": {
-            "shared_storage_path": "'"$SHARED_STORAGE_PATH"'",
-            "ec_max_num_scheduled_tokens": "4096"
-        }
-    }' \
-    >"$ENC_LOG" 2>&1 &
+start_encoder_worker() {
+    ASCEND_RT_VISIBLE_DEVICES="$GPU_E" vllm serve "$MODEL" \
+        --gpu-memory-utilization 0.0 \
+        --port "$ENCODE_PORT" \
+        --enable-request-id-headers \
+        --no-enable-prefix-caching \
+        --max-num-seqs 128 \
+        --max-model-len 3072 \
+        --max-num-batched-tokens 40000 \
+        --enforce-eager \
+        --ec-transfer-config '{
+            "ec_connector": "ECSharedStorageConnector",
+            "ec_role": "ec_producer",
+            "ec_connector_extra_config": {
+                "shared_storage_path": "'"$SHARED_STORAGE_PATH"'",
+                "ec_max_num_scheduled_tokens": "4096"
+            }
+        }' \
+        >"$ENC_LOG" 2>&1 &
 
-echo $! >> "$PID_FILE"
+    echo $! >> "$PID_FILE"
+}
+
+start_encoder_worker
 
 ###############################################################################
 # Prefill / decode worker
 ###############################################################################
-CUDA_VISIBLE_DEVICES="$GPU_PD" vllm serve "$MODEL" \
-    --gpu-memory-utilization 0.7 \
-    --port "$PREFILL_DECODE_PORT" \
-    --enable-request-id-headers \
-    --max-num-seqs 128 \
-    --enforce-eager \
-    --ec-transfer-config '{
-        "ec_connector": "ECSharedStorageConnector",
-        "ec_role": "ec_consumer",
-        "ec_connector_extra_config": {
-            "shared_storage_path": "'"$SHARED_STORAGE_PATH"'"
-        }
-    }' \
-    >"$PD_LOG" 2>&1 &
+start_prefill_decode_worker() {
+    ASCEND_RT_VISIBLE_DEVICES="$GPU_PD" vllm serve "$MODEL" \
+        --gpu-memory-utilization 0.95 \
+        --port "$PREFILL_DECODE_PORT" \
+        --no-enable-prefix-caching \
+        --enable-request-id-headers \
+        --max-num-seqs 128 \
+        --max-model-len 3072 \
+        --max-num-batched-tokens 40000 \
+        --enforce-eager \
+        --ec-transfer-config '{
+            "ec_connector": "ECSharedStorageConnector",
+            "ec_role": "ec_consumer",
+            "ec_connector_extra_config": {
+                "shared_storage_path": "'"$SHARED_STORAGE_PATH"'"
+            }
+        }' \
+        >"$PD_LOG" 2>&1 &
 
-echo $! >> "$PID_FILE"
+    echo $! >> "$PID_FILE"
+}
+
+start_prefill_decode_worker
 
 # Wait until both workers are ready
-wait_for_server "$ENCODE_PORT"
-wait_for_server "$PREFILL_DECODE_PORT"
+# wait_for_server "$ENCODE_PORT"
+# wait_for_server "$PREFILL_DECODE_PORT"
 
 ###############################################################################
 # Proxy
 ###############################################################################
-python /path/to/vllm/vllm/draft/proxy.py \
-    --host "127.0.0.1" \
-    --port "$PROXY_PORT" \
-    --encode-servers-urls "http://localhost:$ENCODE_PORT" \
-    --prefill-decode-servers-urls "http://localhost:$PREFILL_DECODE_PORT" \
-    >"$PROXY_LOG" 2>&1 &
+start_proxy() {
+    python /path/to/vllm/vllm/draft/proxy.py \
+        --host "127.0.0.1" \
+        --port "$PROXY_PORT" \
+        --encode-servers-urls "http://localhost:$ENCODE_PORT" \
+        --prefill-decode-servers-urls "http://localhost:$PREFILL_DECODE_PORT" \
+        >"$PROXY_LOG" 2>&1 &
 
-echo $! >> "$PID_FILE"
+    echo $! >> "$PID_FILE"
+}
 
-wait_for_server "$PROXY_PORT"
+start_proxy
+
+# wait_for_server "$PROXY_PORT"
 echo "All services are up!"
